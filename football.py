e9cd375550255139cc8c3cd880ddf787ff447cf3
JohnnyMango <johnnydrodriguez@gmail.com>
2017-11-12 14:39:02 -0500

    In Progress

--- football.py
+++ football.py
@@ -10,6 +10,15 @@ url = "https://www.cbssports.com/nfl/stats/playersort/nfl/year-2017-season-regul
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, "lxml")
 
-table = soup.findAll("tr")
+table = soup.findAll("table", attrs={"class":"data"})[0].findAll('tr', attrs={"valign":"top"})
 
-print table
+list = []
+for i in table:
+    name = (i.findAll('td')[0].findAll('a')[0].contents[0])
+    list.append(name)
+
+print list
+
+df1 = pd.DataFrame(list)
+
+print df1
